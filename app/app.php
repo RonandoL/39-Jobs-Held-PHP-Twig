cfa70d1da5cb2dd903ceca684b8136a8cbe2d064
Ronando Long <ronando.long@gmail.com>
2017-02-02 18:23:50 -0800

    add route to home page rendering Twig file jobs.html.twig, it works, goody gumdrops

--- app/app.php
+++ app/app.php
@@ -1,11 +1,11 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/tasks.php";
+    require_once __DIR__."/../src/jobs.php";
 
     session_start();                          // For global variable, saving in browser cache
-    if (empty($_SESSION['array_of_tasks'])) {
-        $_SESSION['array_of_tasks'] = array();
+    if (empty($_SESSION['array_of_jobs'])) {
+        $_SESSION['array_of_jobs'] = array();
     }
 
     $app = new Silex\Application();
@@ -16,22 +16,20 @@
   // End Red Tape
 
   // 1. Route for home page
-    $app->get("/", function() use ($app) {
-        return xxxxxxxxxxxx;
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('jobs.html.twig', array('jobs' => Job::getAll()));
     });
 
   // 2. Route for sending instantiated new object (new task) to /tasks URL
-    $app->post("/tasks", function() use ($app) {
-        $task = new xxxxxxxxxxxx;     // Instantiation
-        $save = xxxxxx;
-        return $app['twig']->render('xxxxxx', xxxxxxxxxxxx;
-    });
+    // $app-
+    //     xxxxxxx;
+    // });
 
   // 3. Route for deleting all tasks
-    $app->post('/delete', function() use ($app) {
-        xxxxxxxxxxxx;
-        return $app['twig']->render('xxxxxx');
-    });
+    // $app->post('/e', funce () {
+    //     xxxxxxxxxxxx;
+    //     r ('');
+    // });
 
     return $app;
 
