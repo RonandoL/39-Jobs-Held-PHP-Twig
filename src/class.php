730eee77c11da2ce963d857a4d5b58f95a025001
Ronando Long <ronando.long@gmail.com>
2017-02-02 17:43:47 -0800

    add Class with properties, getters, setters, etc.

--- src/class.php
+++ src/class.php
@@ -1,8 +1,41 @@
 <?php
     class Job
     {
-        private $xxx;
+        private $title;
 
+        function __construct($job_title)
+        {
+            $this->title = $job_title;
+        }
+
+          // Save Job (Instantiation)
+          function save()
+          {
+              array_push($_SESSION['array_of_jobs'], $this);
+          }
+
+          // GET All Jobs (array)
+          static function getAll()
+          {
+              return $_SESSION['array_of_jobs'];
+          }
+
+          // DELETE All Jobs (Empty Array)
+          static function deleteAll()
+          {
+              $_SESSION['array_of_jobs'] = array();
+          }
+
+
+        // JOB TITLE Setter & Getter
+        function setTitle($new_title)
+        {
+            $this->title = $new_title;
+        }
+        function getTitle()
+        {
+            return $this->title;
+        }
 
     }
 ?>
